999cbde70e1f57cf76acd9f107fcf694d8c2bd1e
Chandana Pathipati <chandanachowdary77@gmail.com>
2025-09-23 18:41:32 +0100

    Execution Time

--- date.sh
+++ date.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-DATE=$(date)
-echo "Timestamp is : $DATE"
+#DATE=$(date)
+#echo "Timestamp is : $DATE"
 
 #Use this command for start time,end time and execution time and saved it in the log file.
 #In this date command,for more options to display the time by using "date --help".
+
+START_TIME=$(date +%S)
+sleep 10
+END_TIME=$(date +%S)
+TOTAL_EXEC_TIME=(($END_TIME-$START_TIME))
+echo "Total Execution Time is: $TOTAL_EXEC_TIME"
