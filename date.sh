ed86f3493bc6f277923512ffaf103cdc50bb770e
Chandana Pathipati <chandanachowdary77@gmail.com>
2025-09-23 18:29:19 +0100

    date.sh

---
+++ date.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+DATE=$(date)
+echo "Timestamp is : $DATE"
+
+#Use this command for start time,end time and execution time and saved it in the log file.
+#In this date command,for more options to display the time by using "date --help".
