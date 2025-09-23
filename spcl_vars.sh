29ed5b15e720a2bec1435ba84cd803c09ca9b8b6
Chandana Pathipati <chandanachowdary77@gmail.com>
2025-09-23 19:20:13 +0100

    Special Variables

---
+++ spcl_vars.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "All variables passed to the script: $@"
+echo "All variables passed to the script: $*"
+echo "Script Name: $0"
+echo "Current Directory: $PWD"
+echo "Who is running this: $USER"
+echo "Home Directory: $HOME" 
