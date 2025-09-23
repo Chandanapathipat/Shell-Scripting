846cee693d37ea36df8239e31828e64b00437248
Chandana Pathipati <chandanachowdary77@gmail.com>
2025-09-23 19:28:19 +0100

    Special Variables

--- spcl_vars.sh
+++ spcl_vars.sh
@@ -4,4 +4,7 @@ echo "All variables passed to the script: $*"
 echo "Script Name: $0"
 echo "Current Directory: $PWD"
 echo "Who is running this: $USER"
-echo "Home Directory: $HOME" 
+echo "Home Directory of User: $HOME" 
+echo "PID of this Script: $$"
+sleep 50 &
+echo "PID of the last command in background is: $!"
