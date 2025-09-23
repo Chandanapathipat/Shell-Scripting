1c2895bce1fa8770ef481260f3f799713e5115be
Chandana Pathipati <chandanachowdary77@gmail.com>
2025-09-23 18:46:35 +0100

    Execution Time

--- date.sh
+++ date.sh
@@ -5,8 +5,8 @@
 #Use this command for start time,end time and execution time and saved it in the log file.
 #In this date command,for more options to display the time by using "date --help".
 
-START_TIME=$(date +%S)
+START_TIME=$(date +%s)
 sleep 15
-END_TIME=$(date +%S)
+END_TIME=$(date +%s)
 TOTAL_EXEC_TIME=($END_TIME - $START_TIME)
 echo "Total Execution Time is: $TOTAL_EXEC_TIME"
