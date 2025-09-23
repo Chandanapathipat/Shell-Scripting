2f8b64b286d01e24d4aa7ded092cd6fbe374f8f3
Chandana Pathipati <chandanachowdary77@gmail.com>
2025-09-23 14:06:58 +0100

    adding -s

--- read.sh
+++ read.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Enter your PIN Number:"
-read PIN_NUMBER  #PIN_NUMBER is variable here
+read -s PIN_NUMBER  #PIN_NUMBER is variable here
+#-s goal is to invisible the entered text at the time of output
 echo "Your Entered Pin is:$PIN_NUMBER"
