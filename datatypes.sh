9c7f97760be2143064e343c0b04c35c7f0b82f8f
Chandana Pathipati <chandanachowdary77@gmail.com>
2025-09-24 11:48:14 +0100

    Datatypes

--- datatypes.sh
+++ datatypes.sh
@@ -4,4 +4,5 @@ Num1=100
 Num2=200
 Num3="DevOps"
 Sum=$(($Num1+$Num2+$Num3))
+#The result shows 300 because in shell script there are no data types and here the string is taken as 0
 echo "The Sum is:$Sum"
