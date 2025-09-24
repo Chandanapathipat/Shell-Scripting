329a7b70f7de073b232ab48e8faee2540344770d
Chandana Pathipati <chandanachowdary77@gmail.com>
2025-09-24 12:58:49 +0100

    installing packages using function

---
+++ function.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+User_id=$(id -u)
+
+if [ $User_id -ne 0 ]; then
+    echo "Error:Please run this Script with root privilege"
+    exit 1 #failure is other than 0
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "Error:Installing $2 is failure"
+        exit 1
+    else 
+        echo "Installing $2 is SUCCESS"
+    fi
+}
+
+dnf install mysql -y
+VALIDATE $? "MySQL"
+
+dnf install nginx -y
+VALIDATE $? "Nginx"
+
+dnf install mongodb-mongossh -y
+VALIDATE $? "Mongosh"
