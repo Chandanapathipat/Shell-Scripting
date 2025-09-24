99ab93b78d33b43f28d90bcea1e1d2d7ba8eb234
Chandana Pathipati <chandanachowdary77@gmail.com>
2025-09-24 12:09:45 +0100

    condition.sh

--- conditions.sh
+++ conditions.sh
@@ -2,6 +2,8 @@
 Num1=$1
 if [ $Num1 -lt 10 ]; then
     echo "Given Number $Num1 is less than 10"
+elif [ $Num1 -eq 10 ]; then
+    echo "Given Number $Num1 is equal to 10"
 else
     echo "Given Number $Num1 is greater than 10"
 fi
