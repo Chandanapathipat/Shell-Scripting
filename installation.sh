c6cf64503412ae1d0792de91593a654c5785646e
Chandana Pathipati <chandanachowdary77@gmail.com>
2025-09-24 12:38:52 +0100

    mysql installation

---
+++ installation.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+User_id=$(id -u)
+
+#id -u is used to check the user is root user or not
+
+if [ $User_id -ne 0 ]; then
+    echo "Error:Please run this Script with root privilege"
+    exit 1 #failure is other than 0
+fi
+dnf install mysql -y
+if [ $? -ne 0 ]; then
+    echo "Error:Installing MySQL is failure"
+    exit 1
+else 
+    echo "Installing MySQL is SUCCESS"
+fi
