b8ea3399b9a2604534c7521dcf20b014300eb7d9
Chandana Pathipati <chandanachowdary77@gmail.com>
2025-09-24 13:35:01 +0100

    Adding Colours to the Script

--- colours_1.sh
+++ colours_1.sh
@@ -4,6 +4,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 W="\e[0m"
+
 User_id=$(id -u)
 
 if [ $User_id -ne 0 ]; then
@@ -11,20 +12,34 @@ if [ $User_id -ne 0 ]; then
     exit 1 #failure is other than 0
 fi
 
-VALIDATE(){
+VALIDATE(){ #Functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-        echo "Error:Installing $2 is failure"
+        echo -e "Installing $2 is...$R FAILURE $W "
         exit 1
     else 
-        echo "Installing $2 is SUCCESS"
+        echo -e "Installing $2 is...$R SUCCESS $W"
     fi
 }
 
-dnf install mysql -y
-VALIDATE $? "MySQL"
+dnf list installed mysql
+#Install if is not found
+if [ $? -ne 0 ]; then
+    dnf install mysql -y
+    VALIDATE $? "MySQL"
+else
+    echo -e "MySQL already exist...$Y SKIPPING $W"
+fi
 
-dnf install nginx -y
-VALIDATE $? "Nginx"
+if [ $? -ne 0 ]; then
+    dnf install nginx -y
+    VALIDATE $? "Nginx"
+else
+    echo -e "Nginx already exist...$Y SKIPPING $W"
+fi
 
-dnf install python3 -y
-VALIDATE $? "Python"
+if [ $? -ne 0 ]; then
+    dnf install python -y
+    VALIDATE $? "Python"
+else
+    echo -e"Python already exist...$Y SKIPPING $W"
+fi
