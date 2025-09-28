3f46d776e21c3b69369bbd6d94dd3fe60c063423
Chandana Pathipati <chandanachowdary77@gmail.com>
2025-09-28 10:30:58 +0100

    For Loop added to the script

--- loop1.sh
+++ loop1.sh
@@ -4,8 +4,15 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 W="\e[0m"
+Logs_Folder="/var/log/shell-script"
+Script_Name=$( echo $0 | cut -d "." -f1 )
+#Here $0 gives the script name
+Log_File="$Logs_Folder/$Script_Name.log"
 
 User_id=$(id -u)
+
+mkdir -p $Logs_Folder
+echo "Script started executed at:$(date)"  | tee -a $Log_File                               
 
 if [ $User_id -ne 0 ]; then
     echo "Error:Please run this Script with root privilege"
@@ -14,14 +21,23 @@ fi
 
 VALIDATE(){ #Functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-        echo -e "Installing $2 is...$R FAILURE $W "
+        echo -e "Installing $2 is...$R FAILURE $W " | tee -a $Log_File 
         exit 1
     else 
-        echo -e "Installing $2 is...$R SUCCESS $W"
+        echo -e "Installing $2 is...$R SUCCESS $W" | tee -a $Log_File 
     fi
 }
 
-for package in {$@}
+for package in $@
 do
-    echo "Package is:$package"
+    #check package is installed or not
+    dnf list installed $package &>>$Log_File
+
+    #if exit status is 0,already installed. -ne 0 need to install it
+    if [ $? -ne 0 ]; then
+        dnf install $package -y &>>Log_File
+        validate $? "$package"
+    else
+        echo "$package already installed...$Y SKIPPING $W"
+    fi
 done
